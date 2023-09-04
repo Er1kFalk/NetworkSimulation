9fc004e46e0a9853ab4e144e19409f52814140f6
erik <erik@Ubuntu.myguest.virtualbox.org>
2023-09-04 11:48:30 +0200

    Created functions that can open .PCAP file and count nr of packets.

--- src/NetworkSim.cpp
+++ src/NetworkSim.cpp
@@ -7,9 +7,19 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include <pcap.h>
+#include "PCAPReader.h"
+
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	char* filepath = "./src/test_trace.pcapng";
+	try {
+		count_packets(filepath);
+	}
+	catch (std::invalid_argument& e) {
+		std::cerr << e.what() << std::endl;
+		return -1;
+	}
+
 	return 0;
 }
---
+++ src/PCAPReader.h
@@ -0,0 +1,14 @@
+/*
+ * PCAPReader.h
+ *
+ *  Created on: 4 Sept 2023
+ *      Author: erik
+ */
+
+#ifndef PCAPREADER_H_
+#define PCAPREADER_H_
+
+pcap_t* open_pcap_file(char* file_path);
+void count_packets(char* filepath);
+
+#endif /* PCAPREADER_H_ */
