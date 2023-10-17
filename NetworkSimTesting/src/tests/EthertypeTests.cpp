5099eea6bc73cc2516b89608584b6b60365e0079
erik <erik@Ubuntu.myguest.virtualbox.org>
2023-10-17 13:50:41 +0200

    Restructured ethertype source files in file hierarchy and added automated testing for ethertype.

---
+++ NetworkSimTesting/src/tests/EthertypeTests.cpp
@@ -0,0 +1,99 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <vector>
+#include <sstream>
+#include <string>
+
+#include "EthertypeTests.h"
+#include "Ethertype/EthertypeInterface.h"
+#include "Ethertype/Ethertype.h"
+#include "../TestUtils/TestUtils.h"
+
+TEST(EthertypeInterface, set_get_ethertype_vector) {
+    EthertypeInterface *e = new Ethertype;
+
+    std::vector<std::vector<unsigned char>> correct_ethertypes = {
+        {0x01, 0x02},
+        {0x11, 0xAB},
+        {0x21, 0x0B},
+        {0xFF, 0xD2}
+    };
+
+    std::vector<std::vector<unsigned char>> wrong_ethertypes = {
+        {0x01}, // too short
+        {0x01, 0x02, 0x03}, // too long
+        {0x04, 0x05, 0xAB, 0x04} 
+    };
+
+    for (std::vector<unsigned char> eth : correct_ethertypes) {
+        e->set_ethertype(eth);
+        EXPECT_THAT(e->get_ethertype(), testing::ElementsAreArray(eth)) << TestUtils::generate_error_msg("Correct Ethertype", "Ethertype should have been set, but wasn't");
+    }
+
+    for (std::vector<unsigned char> eth : wrong_ethertypes) {
+        try {
+            e->set_ethertype(eth);
+            FAIL();
+        } catch (std::invalid_argument exc) {
+            std::ostringstream expected_err;
+            expected_err << "Ethertype should be " << e->get_octet_length() << " octets, but was " << eth.size() << " octets";
+            // EXPECT_STREQ(exc.what(), expected_err.str().c_str());
+            EXPECT_THAT(exc.what(), testing::StrEq(expected_err.str()));
+        }
+    }
+
+    delete e;
+}
+
+TEST(EthertypeInterface, set_get_ethertype_string) {
+    EthertypeInterface *e = new Ethertype;
+    
+    std::vector<std::string> accepted_strings = {
+        "ipv4",
+        "ipv6",
+        "arp",
+        "iPv4",
+        "IPv4",
+        "ArP",
+        "ARP",
+        "ipv6"
+    };
+
+    std::vector<std::vector<unsigned char>> expected_ethertype_values = {
+        {0x08, 0x00}, // ipv4
+        {0x86, 0xDD}, // ipv6
+        {0x08, 0x06}, // arp
+        {0x08, 0x00}, // iPv4
+        {0x08, 0x00}, // IPv4
+        {0x08, 0x06}, // ArP
+        {0x08, 0x06}, // ARP
+        {0x86, 0xDD} // iPv6
+    };
+
+    ASSERT_EQ(accepted_strings.size(), expected_ethertype_values.size());
+
+    std::vector<std::string> wrong_strings = {
+        "ip v4",
+        "IP.v4",
+        "A r P",
+        "asdmn"
+    };
+
+    for (int i = 0; i < accepted_strings.size(); i++) {
+        e->set_ethertype(accepted_strings[i]);
+        EXPECT_THAT(e->get_ethertype(), testing::ElementsAreArray(expected_ethertype_values[i]));
+        e->set_ethertype((std::vector<unsigned char>){0x00, 0x00}); // reset ethertype for next test
+    }
+
+    for (std::string s : wrong_strings) {
+        try {
+            e->set_ethertype(s);
+            FAIL();
+        } catch (std::invalid_argument exc) {
+            std::ostringstream expected_err;
+            expected_err << "Conversion from string '" << s << "' to ethertype value does not exist.";
+            EXPECT_THAT(exc.what(), testing::StrEq(expected_err.str()));
+        }
+    }
+}
---
+++ NetworkSimTesting/src/tests/EthertypeTests.h
@@ -0,0 +1,15 @@
+/*
+ * EthertypeTests.h
+ *
+ *  Created on: 17 Oct 2023
+ *      Author: erik
+ */
+
+#ifndef ETHERTYPETESTS_H_
+#define ETHERTYPETESTS_H_
+
+
+
+
+
+#endif /* ETHERTYPETESTS_H_ */
