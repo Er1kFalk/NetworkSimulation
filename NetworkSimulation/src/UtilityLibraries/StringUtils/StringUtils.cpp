c8fa71d9e15c4a04f1e633d5dcdecb9fbca6fd8e
Erik Falk <s204438@student.dtu.dk>
2024-06-07 20:22:23 +0200

    removed unnecessary functions

--- NetworkSimulation/src/UtilityLibraries/StringUtils/StringUtils.cpp
+++ NetworkSimulation/src/UtilityLibraries/StringUtils/StringUtils.cpp
@@ -18,28 +18,6 @@ std::string StringUtils::to_lower(std::string s) {
     return to_lower;
 }
 
-bool StringUtils::is_hex_symbol(char c) {
-    bool is_hex_letter = ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F');
-    bool is_number = '0' <= c && c <= '9';
-
-
-    return (is_hex_letter || is_number);
-}
-
-// returns true if in s, the char "repeats" after count chars
-// example: s="abc:def:ghi", repeats=':', count=3 returns true, whereas if s="abcd:efgh" it would return false
-bool StringUtils::char_repeats_in_string(std::string s, char repeats, std::string::size_type count) {
-    if (s.length() < count) {
-        return false;
-    }
-    for (std::string::size_type i = count; i < s.length(); i+=count+1) {
-        if (s[i] != repeats) {
-            return false;
-        }
-    }
-    return true;
-}
-
 // finite automata that returns true if IP is valid
 bool StringUtils::is_valid_ipv4(std::string ipv4) {
     std::map<std::tuple<int, std::string>, int> trans_function = {
